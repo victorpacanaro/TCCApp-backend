0a40eae0ebcb3ef3b5598174c058b48c62ad2b70
Victor Contre Pacanaro <victor.contre@MIDGARD.LOCAL>
2020-10-04 11:42:39 -0300

    Projeto criado

---
+++ src/main/java/com/victorpacanaro/TCCApp/TccAppApplication.java
@@ -0,0 +1,13 @@
+package com.victorpacanaro.TCCApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorpacanaro/TCCApp/TccAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorpacanaro.TCCApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
