3389b14b139bccdfca912b86a85a5e844bd682e7
Victor Pacanaro <victorcontrepacanaro@outlook.com>
2020-10-25 10:47:47 -0300

    Primeira Classe de Domínio - Rotas

--- src/main/java/com/victorpacanaro/TCCApp/resources/RotaResource.java
+++ src/main/java/com/victorpacanaro/TCCApp/resources/RotaResource.java
@@ -1,16 +1,29 @@
 package com.victorpacanaro.TCCApp.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.victorpacanaro.TCCApp.domain.Rotas;
+
 @RestController
-@RequestMapping(value="/rotas")
+@RequestMapping(value="/rotas") //endpoint rest 
 public class RotaResource {
 	
 	@RequestMapping (method = RequestMethod.GET)
-	public String listar () {
-		return "REST está funcionando";
+	public List<Rotas> listar() {
+		
+		Rotas rot1 = new Rotas(1, "Rota A");
+		Rotas rot2 = new Rotas(2, "Rota B");
+		
+		List<Rotas> lista = new ArrayList<>();
+		lista.add(rot1);
+		lista.add(rot2);
+		
+		return lista;
 		
 	}
 
